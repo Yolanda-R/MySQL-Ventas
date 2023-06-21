bc825afbb1e66bd232b832cac415794866759a02
Yolanda Rojas <lyolanda693@outlook.com>
2023-06-21 10:00:04 -0500

    Tercer avance con Query

--- Excersice_02.sql
+++ Excersice_02.sql
@@ -145,3 +145,62 @@ JOIN MANUFACTURER M ON P.ID_FABRICANTE=M.ID
 WHERE P.PRECIO >= 180 
 ORDER BY P.PRECIO DESC , M.NOMBRE ASC;
 #Devuelve un listado con el identificador y el nombre de fabricante, solamente de aquellos fabricantes que tienen productos asociados en la base de datos.
+SELECT  M.ID,M.NOMBRE AS NOM_FAB, P.Id_fabricante FROM MANUFACTURER M JOIN PRODUCT P ON M.ID=P.ID_FABRICANTE;
+#Devuelve un listado de todos los fabricantes que existen en la base de datos, junto con los productos que tiene cada uno de ellos. El listado deberá mostrar también aquellos fabricantes que no tienen productos asociados.
+SELECT M.NOMBRE AS NOM_FAB, P.NOMBRE AS NOM_PROD FROM MANUFACTURER M LEFT JOIN PRODUCT P ON M.ID = P.ID_FABRICANTE;
+#Devuelve un listado donde sólo aparezcan aquellos fabricantes que no tienen ningún producto asociado.
+SELECT M.NOMBRE AS N_FAB, P.NOMBRE AS NOM_PROD FROM MANUFACTURER M LEFT JOIN PRODUCT P ON M.ID =P.ID_FABRICANTE WHERE P.NOMBRE IS NULL;
+#¿Pueden existir productos que no estén relacionados con un fabricante? Justifique su respuesta.
+#Según la query no se evidencia productos que no tengan relacion con algún fabricante 	
+SELECT P.NOMBRE AS N_PROD, M.NOMBRE AS N_FAB FROM PRODUCT P LEFT JOIN MANUFACTURER M ON P.ID_FABRICANTE = M.ID;
+#Calcula el número total de productos que hay en la tabla productos.
+SELECT COUNT(NOMBRE) AS CUENTA_T FROM PRODUCT;
+#Calcula el número total de fabricantes que hay en la tabla fabricante.
+SELECT COUNT(NOMBRE) AS CUENTA FROM MANUFACTURER;
+#Calcula el número de valores distintos de identificador de fabricante aparecen en la tabla productos.
+SELECT COUNT(DISTINCT ID_FABRICANTE) AS FAB_COUN FROM PRODUCT; 
+#Calcula la media del precio de todos los productos.
+SELECT AVG(ROUND(PRECIO)) AS PROMEDIO FROM PRODUCT;
+#Calcula el precio más barato de todos los productos.
+SELECT MIN(PRECIO) FROM PRODUCT;
+#Calcula el precio más caro de todos los productos.
+SELECT MAX(PRECIO) FROM PRODUCT;
+#Lista el nombre y el precio del producto más barato.
+SELECT NOMBRE, PRECIO FROM PRODUCT ORDER BY PRECIO ASC LIMIT 1;
+#Lista el nombre y el precio del producto más caro.
+
+#Calcula la suma de los precios de todos los productos.
+
+#Calcula el número de productos que tiene el fabricante Asus.
+
+#Calcula la media del precio de todos los productos del fabricante Asus.
+
+#Calcula el precio más barato de todos los productos del fabricante Asus.
+
+#Calcula el precio más caro de todos los productos del fabricante Asus.
+
+#Calcula la suma de todos los productos del fabricante Asus.
+
+#Muestra el precio máximo, precio mínimo, precio medio y el número total de productos que tiene el fabricante Crucial.
+
+#Muestra el número total de productos que tiene cada uno de los fabricantes. El listado también debe incluir los fabricantes que no tienen ningún producto. El resultado mostrará dos columnas, una con el nombre del fabricante y otra con el número de productos que tiene. Ordene el resultado descendentemente por el número de productos.
+
+#Muestra el precio máximo, precio mínimo y precio medio de los productos de cada uno de los fabricantes. El resultado mostrará el nombre del fabricante junto con los datos que se solicitan.
+
+#Muestra el precio máximo, precio mínimo, precio medio y el número total de productos de los fabricantes que tienen un precio medio superior a 200€. No es necesario mostrar el nombre del fabricante, con el identificador del fabricante es suficiente.
+
+#Muestra el nombre de cada fabricante, junto con el precio máximo, precio mínimo, precio medio y el número total de productos de los fabricantes que tienen un precio medio superior a 200€. Es necesario mostrar el nombre del fabricante.
+
+#Calcula el número de productos que tienen un precio mayor o igual a 180€.
+
+#Calcula el número de productos que tiene cada fabricante con un precio mayor o igual a 180€.
+
+#Lista el precio medio los productos de cada fabricante, mostrando solamente el identificador del fabricante.
+
+#Lista el precio medio los productos de cada fabricante, mostrando solamente el nombre del fabricante.
+
+#Lista los nombres de los fabricantes cuyos productos tienen un precio medio mayor o igual a 150€.
+
+#Devuelve un listado con los nombres de los fabricantes que tienen 2 o más productos.
+
+#Devuelve un listado con los nombres de los fabricantes y el número de productos que tiene cada uno con un precio superior o igual a 220 €. No es necesario mostrar el nombre de los fabricantes que no tienen productos que cumplan la condición.
